85960278f8796be77cf461736628888202764974
cptnbrando <bdcruz128@gmail.com>
2021-05-17 17:31:22 -0500

    Adding Spotify Playback SDK

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,3 +1,5 @@
+/// <reference types="@types/spotify-web-playback-sdk"/>
+
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
 
@@ -14,6 +16,7 @@ export class PlayerComponent implements OnInit {
   @Input() showPlaylistBar: boolean;
   @Input() showStationBar: boolean;
   @Input() isPlaying: boolean;
+  @Input() accessToken: string;
 
   @Output() toggleBarEvent = new EventEmitter<number>();
   @Output() isPlayingEvent = new EventEmitter<boolean>();
@@ -21,6 +24,7 @@ export class PlayerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.spotifyWebSDKInit();
   }
 
   toggleBar(bar: number) {
@@ -29,6 +33,40 @@ export class PlayerComponent implements OnInit {
 
   togglePlay() {
     this.isPlayingEvent.emit(true);
+  }
+
+  spotifyWebSDKInit(): void {
+    // Initializing the web sdk player
+    // https://www.npmjs.com/package/@types/spotify-web-playback-sdk
+    // https://developer.spotify.com/documentation/web-playback-sdk/quick-start/
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      const player = new Spotify.Player({
+        name: 'am_radio',
+        getOAuthToken: cb => { cb(this.accessToken); }
+      });
+
+      // Error handling
+      player.addListener('initialization_error', ({ message }) => { console.error(message); });
+      player.addListener('authentication_error', ({ message }) => { console.error(message); });
+      player.addListener('account_error', ({ message }) => { console.error(message); });
+      player.addListener('playback_error', ({ message }) => { console.error(message); });
+
+      // Playback status updates
+      player.addListener('player_state_changed', state => { console.log(state); });
+
+      // Ready
+      player.addListener('ready', ({ device_id }) => {
+        console.log('Ready with Device ID', device_id);
+      });
+
+      // Not Ready
+      player.addListener('not_ready', ({ device_id }) => {
+        console.log('Device ID has gone offline', device_id);
+      });
+
+      // Connect to the player!
+      player.connect();
+    }
   }
 
 }
